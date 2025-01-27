49d846e943cff8fcd78a85d6d9deca8cdd58daec
Christoph Zirkelbach <christoph@tigion.de>
2025-01-27 14:22:13 +0100

    fix: ignore long execution output in execSync() (issue #11)

--- server/controllers/asciidoc.js
+++ server/controllers/asciidoc.js
@@ -71,7 +71,7 @@ function convertWithAsciidoctorCmd(file, cacheDir) {
   //
   // convert mit Asciidoctor command
   const childProcess = require("child_process");
-  const stdout = childProcess.execSync(cmd);
+  const stdout = childProcess.execSync(cmd, { stdio: "ignore" });
 
   // add script for client registration and refresh event
   // (not perfect, but it works)
