57eef68443c90a2f6ad0f109b370edc53733f132
Christoph Zirkelbach <christoph@tigion.de>
2025-09-29 11:59:06 +0200

    fix(server): allow dotfiles for node express to prevent `NotFoundError: Not Found` error
    - The server runs in Neovim under `.local/share/nvim/`, a dotfile
    directory.

--- server/controllers/index.js
+++ server/controllers/index.js
@@ -7,11 +7,16 @@ const path = require("path");
 const data = require("../data.js");
 const asciidoc = require("../controllers/asciidoc.js");
 
+// The sendFile options
+const options = {
+  dotfiles: "allow",
+};
+
 // send page templates or AsciiDoc preview to clients
 exports.page = (_req, res) => {
   if (data.preview.isFinished) {
     // send server has stopped HTML template
-    res.sendFile(path.join(__dirname, "../templates/stop.html"));
+    res.sendFile(path.join(__dirname, "../templates/stop.html"), options);
   } else if (data.preview.filepath) {
     // send AsciiDoc file converted to HTML
     res.send(
@@ -23,6 +28,6 @@ exports.page = (_req, res) => {
     );
   } else {
     // send server has started HTML template
-    res.sendFile(path.join(__dirname, "../templates/wait.html"));
+    res.sendFile(path.join(__dirname, "../templates/wait.html"), options);
   }
 };
