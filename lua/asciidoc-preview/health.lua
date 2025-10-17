a292835d8842217b1b31b5e4cd25cc1794a00ba7
Christoph Zirkelbach <christoph@tigion.de>
2025-10-17 14:36:56 +0200

    refactor: rework health check and add versions

--- lua/asciidoc-preview/health.lua
+++ lua/asciidoc-preview/health.lua
@@ -15,42 +15,70 @@ local function check_supported_os()
   local os = vim.uv.os_uname().sysname
 
   if vim.fn.has('mac') == 1 then
-    ok('macOS (' .. os .. ') supported')
+    ok('macOS (`' .. os .. '`) is supported')
   elseif vim.fn.has('linux') == 1 then
     -- NOTE: NixOS is not yet supported!
     --       Write permissions of the plugin directory are currently a problem on NixOS.
-    ok('Linux (' .. os .. ') supported (with limitations, e.g. NixOS)')
+    ok('Linux (`' .. os .. '`) is supported (with limitations, e.g. NixOS)')
   elseif vim.fn.has('wsl') == 1 then
-    warn('Windows WSL (' .. os .. ') supported (with limitations)')
+    warn('Windows WSL (`' .. os .. '`) is supported (with limitations)')
   else
-    error('Operating system (' .. os .. ') not supported or not yet tested')
+    error('Operating system (`' .. os .. '`) is not supported or not yet tested')
   end
 end
 
+------Checks if the given executable is installed.
+------@param name string The command name without arguments.
+------@param optional? boolean Whether it is optional.
+---local function check_executable(name, optional)
+---  if vim.fn.executable(name) == 1 then
+---    ok(name .. ' installed')
+---  elseif optional or false then
+---    warn(name .. ' not installed')
+---  else
+---    error(name .. ' not installed')
+---  end
+---end
+
 ---Checks if the given executable is installed.
----@param name string The command name without arguments.
----@param optional? boolean Whether it is optional.
-local function check_executable(name, optional)
-  if vim.fn.executable(name) == 1 then
-    ok(name .. ' installed')
-  elseif optional or false then
-    warn(name .. ' not installed')
+---@param cmd_name string The command name without arguments.
+---@param opts? {optional?: boolean, show_version?: boolean, version_cmd?: string} Additional options.
+local function check_executable(cmd_name, opts)
+  opts = opts or {}
+  if opts.optional == nil then opts.optional = false end
+  if opts.show_version == nil then opts.show_version = true end
+
+  if vim.fn.executable(cmd_name) == 1 then
+    local cmd_version = nil
+    if opts.show_version then
+      local version_cmd = opts.version_cmd or (cmd_name .. ' --version')
+      local version = (vim.fn.systemlist(version_cmd)[1] or '')
+      if #version > 80 then version = version:sub(1, 80) .. '…' end
+      cmd_version = '`' .. (version ~= '' and version or 'unknown version') .. '`'
+    end
+    ok(cmd_name .. ': ' .. (cmd_version or 'is installed'))
+  elseif opts.optional then
+    warn(cmd_name .. ': is not installed')
   else
-    error(name .. ' not installed')
+    error(cmd_name .. ': is not installed')
   end
 end
 
 ---Checks if the given ruby gem is installed.
----@param name string The gem name.
----@param optional? boolean Whether it is optional.
-local function check_ruby_gem(name, optional)
-  vim.fn.system('gem list --no-version | grep -q "^' .. name .. '$"')
+---@param gem_name string The gem name.
+---@param opts? {optional?: boolean} Additional options.
+local function check_ruby_gem(gem_name, opts)
+  opts = opts or {}
+  if opts.optional == nil then opts.optional = false end
+
+  -- vim.fn.system('gem list --no-version | grep -q "^' .. gem_name .. '$"')
+  local version = vim.fn.system('gem list | grep "^' .. gem_name .. ' .*$"'):match('%((.*)%)')
   if vim.v.shell_error == 0 then
-    ok(name .. ' gem installed')
-  elseif optional or false then
-    warn(name .. ' gem not installed')
+    ok('gem ' .. gem_name .. ': `' .. (version or 'unknown version') .. '`')
+  elseif opts.optional then
+    warn('gem ' .. gem_name .. ': not installed')
   else
-    error(name .. ' gem not installed')
+    error('gem ' .. gem_name .. ': not installed')
   end
 end
 
@@ -95,11 +123,13 @@ end
 
 ---Checks required Asciidoctor tools.
 local function check_asciidoctor()
-  info("The following warnings or errors can be ignored\nif Asciidoctor.js (`converter = 'js'`) is used.")
-  check_executable('asciidoctor', true)
-  check_executable('ruby', true)
-  check_ruby_gem('asciidoctor', true)
-  check_ruby_gem('asciidoctor-diagram', true)
+  info("The following warnings or errors can be ignored if Asciidoctor.js (`converter = 'js'`) is used.")
+  check_executable('ruby', { optional = true })
+  check_executable('asciidoctor', { optional = true })
+  check_ruby_gem('asciidoctor', { optional = true })
+  check_ruby_gem('asciidoctor-diagram', { optional = true })
+  -- check_ruby_gem('asciidoctor-diagram-plantuml', true)
+  info('More ruby gems can be needed depending on the used Asciidoctor extensions.')
 end
 
 ---Checks the health of the plugin.
